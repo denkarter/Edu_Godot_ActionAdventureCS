5f7133e14f3d784bd4a5bb051f9487763c663585
Denis Vasimov <dvasimov@gmail.com>
2024-05-06 10:21:28 +0500

    Added player moving

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -4,6 +4,7 @@ namespace ActionAdventureCS.Scripts.Characters.Player
 {
     public class Player : KinematicBody
     {
+        private Vector2 _direction = new Vector2();
         // Declare member variables here. Examples:
         // private int a = 2;
         // private string b = "text";
@@ -14,7 +15,20 @@ namespace ActionAdventureCS.Scripts.Characters.Player
         
         }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _PhysicsProcess(float delta)
+        {
+            base._PhysicsProcess(delta);
+            Vector3 velocity = new Vector3(_direction.x, 0, _direction.y) * 5;
+            MoveAndSlide(velocity);
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            base._Input(@event);
+            _direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        }
+
+        //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      

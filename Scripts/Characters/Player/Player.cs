2ba94965ef0b844f4d53dfe12b157d7a44f8ace9
Denis Vasimov <dvasimov@gmail.com>
2024-05-07 20:26:38 +0500

    Added flip

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -7,13 +7,14 @@ namespace ActionAdventureCS.Scripts.Characters.Player
     {
         // [ExportGroup("Required Nodes")]
         private AnimationPlayer _animationPlayerNode;
-        
+        private Sprite3D _spriteNode;
         private Vector2 _direction = new Vector2();
         
         public override void _Ready()
         {
             base._Ready();
             _animationPlayerNode = this.GetNode<AnimationPlayer>("AnimationPlayer");
+            _spriteNode = this.GetNode<Sprite3D>("Sprite3D");
             //GD.Print(_animationPlayerNode.Name);
             _animationPlayerNode.Play(PlayerConstants.AnimIdle);
         }
@@ -27,6 +28,8 @@ namespace ActionAdventureCS.Scripts.Characters.Player
                 _animationPlayerNode.Play(PlayerConstants.AnimIdle);
             else
                 _animationPlayerNode.Play(PlayerConstants.AnimMove);
+            
+            Flip();
         }
 
         public override void _Input(InputEvent @event)
@@ -34,6 +37,16 @@ namespace ActionAdventureCS.Scripts.Characters.Player
             base._Input(@event);
             _direction = Input.GetVector(PlayerConstants.InputMoveLeft, PlayerConstants.InputMoveRight,
                 PlayerConstants.InputMoveForward, PlayerConstants.InputMoveBackward);
+        }
+
+        public void Flip()
+        {
+            bool isNotMovingHorizontally = _direction.x == 0;
+            if (isNotMovingHorizontally)
+                return;
+
+            bool isMovingLeft = _direction.x < 0;
+            _spriteNode.FlipH = isMovingLeft;
         }
 
         //  // Called every frame. 'delta' is the elapsed time since the previous frame.
